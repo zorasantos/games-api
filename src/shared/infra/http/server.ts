808ad509ca71413e6e2e2081bd954a631a4d9668
zorasantos <zoranildo22@gmail.com>
2022-06-18 18:44:19 -0300

    config logger with winston

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 
+import Logger from '@shared/logger';
+
 const app = express();
 app.use(express.json());
 
 app.get('/list-games', (req, res) => {
+  Logger.info('Entrou no list-games');
   res.send({ name: 'pac-man', year: '1981' });
 });
 app.listen(3000);
