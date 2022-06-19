6b52da2a62114026829764db655f40292732127d
zorasantos <zoranildo22@gmail.com>
2022-06-18 23:16:52 -0300

    config sonarqube

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=zorasantos_games-api&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=zorasantos_games-api)
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,9 +3,11 @@ import express from 'express';
 import Logger from '@shared/logger';
 
 const app = express();
+app.disable('x-powered-by');
+
 app.use(express.json());
 
-app.get('/list-games', (req, res) => {
+app.get('/list-games', (_req, res) => {
   Logger.info('Entrou no list-games');
   res.send({ name: 'pac-man', year: '1981' });
 });
