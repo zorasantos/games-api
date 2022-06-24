14670acbc5578aaa2888a9c2bed92731d60d55ce
zorasantos <zoranildo22@gmail.com>
2022-06-24 11:14:46 -0300

    test: implements unit test ListGameController

---
+++ src/modules/games/useCases/listGames/ListGameController.spec.ts
@@ -0,0 +1,25 @@
+import { Request } from 'express';
+
+import { getMockReq, getMockRes } from '@jest-mock/express';
+
+import { ListGamesController } from './ListGamesController';
+
+jest.mock('./ListGamesUseCase.ts');
+
+describe('ListGamesController', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should be able create to list game controller successfully', async () => {
+    const createGameController = new ListGamesController();
+
+    const req: Request = getMockReq();
+    const { res } = getMockRes();
+
+    const response = await createGameController.handle(req, res);
+
+    expect(response).not.toBeNull();
+    expect(response.status).toHaveBeenCalledWith(200);
+  });
+});
